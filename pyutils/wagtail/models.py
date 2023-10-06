a55a36b7f9ed219f2de1cefb34783d3a3dd956c1
Mayank Mahajan <mayankm2089@@gmail.com>
2023-10-06 11:38:48 +0530

    BasePage

--- pyutils/wagtail/models.py
+++ pyutils/wagtail/models.py
@@ -39,18 +39,41 @@ class BasePage(Page):
         """
         if child_has_parent(self):
             return
-        parent_page_class = getattr(self, "parent_page_types")[0]
-        wagtail_page = Page.objects.type(parent_page_class).first()
-        _attr = parent_page_class._meta.verbose_name.replace(" ", "")
-        parent_page = getattr(wagtail_page, _attr)
+        parent_page_class = self.clean_parent_page_models()[0]
+        parent_page = Page.objects.type(parent_page_class).first()
+        _index_page = parent_page_class._meta.verbose_name.replace(" ", "").lower()
+        index_page = getattr(parent_page, _index_page)
         try:
-            parent_page.add_child(instance=self)
+            index_page.add_child(instance=self)
         except NodeAlreadySaved:
             pass
+
+    def save_revision(
+        self,
+        user=None,
+        submitted_for_moderation=False,
+        approved_go_live_at=None,
+        changed=True,
+        log_action=False,
+        previous_revision=None,
+        clean=True,
+    ):
+        if user:
+            self.created_by = user
+        return super(BasePage, self).save_revision(
+            user,
+            submitted_for_moderation,
+            approved_go_live_at,
+            changed,
+            log_action,
+            previous_revision,
+            clean,
+        )
 
     def save(self, clean=True, user=None, log_action=False, **kwargs):
         self.set_title()
         self.set_parent_page()
+        super(BasePage, self).save(clean, user, log_action, **kwargs)
 
     class Meta:
         abstract = True
