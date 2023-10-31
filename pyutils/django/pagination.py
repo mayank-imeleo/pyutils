5a35c924f21663c7108928383e56f85f7784e802
Mayank Mahajan <mayankm2089@@gmail.com>
2023-10-31 12:37:51 +0530

    fix: PageNumberPagination

--- pyutils/django/pagination.py
+++ pyutils/django/pagination.py
@@ -4,3 +4,13 @@ from rest_framework.pagination import PageNumberPagination
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 8
     max_page_size = 8
+
+
+class PageNumberPagination20(PageNumberPagination):
+    page_size = 20
+    max_page_size = 20
+
+
+class PageNumberPagination30(PageNumberPagination):
+    page_size = 30
+    max_page_size = 30
