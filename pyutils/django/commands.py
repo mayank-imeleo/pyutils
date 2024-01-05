b63c1d4f9e3481c0d2433f39dcec9da18281c8c1
Mayank Mahajan <mayankm2089@@gmail.com>
2024-01-05 21:37:45 +0530

    feat: ModelActionCommand

---
+++ pyutils/django/commands.py
@@ -0,0 +1,70 @@
+from typing import List, Union
+
+import pendulum
+from django.core.management import BaseCommand
+from django.db.models import Model, QuerySet
+
+
+class ModelActionCommand(BaseCommand):
+    """
+    Base class for commands that need to load Django settings.
+    Usage:
+    python manage.py <command_name> --id 1 --action <method_name>
+    """
+
+    model_class = None
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--id",
+            type=int,
+            help="Instance ID to execute action command on",
+        )
+        parser.add_argument("--action", type=str, help="Execute command on instance")
+        return parser
+
+    def handle(self, *args, **options):
+        """
+        Load Django settings.
+        """
+        object_id = options.get("id", None)
+        action = options.get("action")
+        if not self.model_class:
+            raise Exception("Model class not specified")
+        if not action:
+            raise Exception("Action not specified")
+
+        if object_id:
+            objs = self.model_class.objects.order_by("id").filter(id=object_id)
+        else:
+            objs = self.model_class.objects.all()
+
+        start_time = pendulum.now()
+        self.run_action(action, objs)
+        end_time = pendulum.now()
+        delta = end_time - start_time
+
+        print(
+            "{}.{} took: {}".format(self.model_class.__name__, action, delta.in_words())
+        )
+
+    def run_action(
+        self,
+        action: str,
+        objs: Union[QuerySet | List[Model]],
+    ):
+        log_callback = getattr(self, f"{action}_log", lambda obj: "")
+        for n, obj in enumerate(objs, 1):
+            try:
+                getattr(self, action)(obj)
+            except Exception as e:
+                print(e)
+                print("Error processing object with id: {}".format(obj.id))
+                raise e
+            print(
+                "{}/{} | {} ID: {} | {}".format(
+                    n, objs.count(), obj.__class__.__name__, obj.id, log_callback(obj)
+                )
+            )
+
+        getattr(self, action, objs)
