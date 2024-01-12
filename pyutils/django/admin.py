a116875cad5862f493d543edfc7607668400ae9a
Mayank Mahajan <mayankm2089@@gmail.com>
2024-01-12 21:23:15 +0530

    fix: UserTimeStampedModelAdmin

--- pyutils/django/admin.py
+++ pyutils/django/admin.py
@@ -33,7 +33,9 @@ class UserTimeStampedModelAdmin(admin.ModelAdmin):
     """
 
     def save_model(self, request, obj, form, change):
-        if not obj.created_by:
+        try:
+            obj.created_by
+        except AttributeError:
             obj.created_by = request.user
         obj.modified_by = request.user
         super().save_model(request, obj, form, change)
