01ad84b0d42da4d5dab5691e0234e7198e46789a
Mayank Mahajan <mayankm2089@@gmail.com>
2024-03-13 18:11:53 +0530

    fix: listFilters.js

--- pyutils/static/js/django/listFilters.js
+++ pyutils/static/js/django/listFilters.js
@@ -1,19 +1,24 @@
 django.jQuery(document).ready(function () {
-  const addFilterButton = () => {
-    django
-      .jQuery("ul.object-tools")
-      .append(
-        "<li><button id='filterToggleButton' class='button bg-gray-100'>Filters</button>",
-      );
-    const $filterToggleButton = django.jQuery("#filterToggleButton");
-    $filterToggleButton.click(onFilterToggleButtonClick);
-    $filterToggleButton.click();
-  };
 
-  const onFilterToggleButtonClick = () => {
-    const $filters = django.jQuery("#changelist-filter");
-    $filters.toggle();
-  };
-  addFilterButton();
-  console.log("listFilters.js loaded");
+    const $listFilters = django.jQuery("#changelist-filter");
+    const addFilterButton = () => {
+        // if listFilters is not found, return
+        if ($listFilters.length === 0) {
+            return;
+        }
+        django
+            .jQuery("ul.object-tools")
+            .append(
+                "<li><button id='filterToggleButton' class='button bg-gray-100'>Filters</button>",
+            );
+        const $filterToggleButton = django.jQuery("#filterToggleButton");
+        $filterToggleButton.click(onFilterToggleButtonClick);
+        $filterToggleButton.click();
+    };
+
+    const onFilterToggleButtonClick = () => {
+        $listFilters.toggle();
+    };
+    addFilterButton();
+    console.log("listFilters.js loaded");
 });
